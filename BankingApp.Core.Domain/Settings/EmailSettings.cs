27f1e81e4268c1a5c00ce4a41529e781781398c0
JuanM-DS <20230676@itla.edu.do>
2024-07-03 17:34:55 -0400

    implementing email services

---
+++ BankingApp.Core.Domain/Settings/EmailSettings.cs
@@ -0,0 +1,17 @@
+﻿namespace BankingApp.Core.Domain.Settings
+{
+    public class EmailSettings
+    {
+        public string EmailFrom { get; set; }
+
+        public string SmtpHost { get; set; }
+
+        public string SmtpPort { get; set; }
+
+        public string SmtpUser { get; set; }
+
+        public string SmtpPassword { get; set; }
+
+        public string DisplayName { get; set; }
+    }
+}
---
+++ BankingApp.Infrastucture.Shared/Services/EmailServices.cs
@@ -0,0 +1,12 @@
+﻿using BankingApp.Core.Application.DTOs.Email;
+using BankingApp.Core.Application.Interfaces.Services;
+
+namespace BankingApp.Infrastructure.Shared.Services
+{
+    public class EmailServices : IEmailServices
+    {
+        public Task SendEmailAsync(EmailRequestDTO request)
+        {
+        }
+    }
+}
