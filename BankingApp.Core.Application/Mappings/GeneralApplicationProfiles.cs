2fb5b7444becaeb209c7b59fc90dd5602f041111
Darlin04 <darlinnieve4@gmail.com>
2024-07-06 07:54:45 -0400

    UpdateAutoMappper

--- BankingApp.Core.Application/Mappings/GeneralApplicationProfiles.cs
+++ BankingApp.Core.Application/Mappings/GeneralApplicationProfiles.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using BankingApp.Core.Application.DTOs.User;
+using BankingApp.Core.Application.ViewModels.Beneficiary;
 using BankingApp.Core.Application.ViewModels.User;
 
 namespace BankingApp.Core.Application.Mappings
@@ -8,12 +9,27 @@ namespace BankingApp.Core.Application.Mappings
     {
         public GeneralApplicationProfiles()
         {
+            #region UserProfile
+            CreateMap<BeneficiaryViewModel, ApplicationUserDTO>()
+               .ForMember(des => des.Id, opt => opt.Ignore())
+               .ForMember(des => des.UserName, opt => opt.Ignore())
+               .ForMember(des => des.Email, opt => opt.Ignore())
+               .ForMember(des => des.EmailConfirmed, opt => opt.Ignore())
+               .ForMember(des => des.IdCard, opt => opt.Ignore())
+               .ForMember(des => des.Status, opt => opt.Ignore())
+               .ForMember(des => des.Roles, opt => opt.Ignore())
+               .ForMember(des => des.PhotoUrl, opt => opt.Ignore())
+               .ReverseMap()
+               .ForMember(des => des.AccountNumber, opt => opt.Ignore())
+               .ForMember(des => des.UserUserName, opt => opt.Ignore());
+
             CreateMap<ApplicationUserDTO, SaveUserViewModel>()
                 .ForMember(des => des.File, obj => obj.Ignore())
                 .ReverseMap();
 
             CreateMap<ApplicationUserDTO, UserViewModel>()
                 .ReverseMap();
+            #endregion
         }
     }
 }
