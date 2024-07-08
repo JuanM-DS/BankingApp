422f2b452d46001a02baa8eb9a145c9765f258af
carloselena <elenaperezcarlosmanuel@outlook.com>
2024-07-08 18:00:53 -0400

    BeneficiaryService Delete method changed to fit the BeneficiaryRepository GetBeneficiary method

--- BankingApp.Core.Application/Interfaces/Services/IBeneficiaryService.cs
+++ BankingApp.Core.Application/Interfaces/Services/IBeneficiaryService.cs
@@ -6,5 +6,6 @@ namespace BankingApp.Core.Application.Interfaces.Services
     public interface IBeneficiaryService : IGenericService<SaveBeneficiaryViewModel, BeneficiaryViewModel, Beneficiary>
     {
         Task<List<BeneficiaryViewModel>> BeneficiariesList();
+        Task Delete(string userName, int accuntNumber);
     }
 }
